17ded42f2e63ba98687c1aac0909d0ba6b035d73
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-08-14 16:40:55 +0530

    file Sysytem

---
+++ Day2/app.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+// Create file with text add
+// fs.writeFile("dummy.txt", "Hey Kese ho ap! m asynchronous hu synchronous ke bad chlta hu ", () => { // asynchronous fn h ye 
+//     console.log("File write Successfully")
+// })
+
+// fs.writeFileSync("dummy.txt2", "Hey to  Kese ho ap! synchronous hu phale m hi chaluga ") // ye synchronous h
+
+// File text read 
+// fs.readFile("dummy.txt2", "utf-8", (err, data) => {
+//     if(err){
+//         console.log(err)
+//     }
+
+//     else {
+//         console.log(data)
+//     }
+// })
+
+// const val = fs.readFileSync("dummy.txt", "utf-8")
+// console.log(val)
+
+// 
+// File code or text add 
+
+// text deleted krna file
+// fs.unlink("dummy.txt2", () => {
+//     console.log("ok")
+// })
+
+// // Rename file name
+// fs.rename("dummy.txt", "data.txt", () => {
+//     console.log("ok")
+// })
+
+// // Create a Folder 
+
+// fs.mkdir("demo", () => {
+//     console.log("Ok")
+// })
+
+// // Deleted Foldeer 
+// fs.rmdir("demo", () => {
+//     console.log("Deleted Foldeer")
+// })
+
