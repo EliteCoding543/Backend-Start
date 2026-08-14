1bf0a5b789a3c6209bcf467a472670ea06018e06
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-08-14 14:27:40 +0530

    Backend

---
+++ Day1/app.js
@@ -0,0 +1,16 @@
+// require ye humesha top par hi hona chiye
+const {SayHi, SayHi2} = require("./server.js") 
+// const val = require("./server.js")
+// console.log(val)
+
+SayHi()
+SayHi2()
+
+console.log("Hello Im From Backend !")
+// Backned m window ki janga par 
+// global / globalThis use krte h obj access ke liye
+
+// console.log(globalThis)
+// console.log(global)
+
+console.log(process.env)
---
+++ Day1/server.js
@@ -0,0 +1,11 @@
+function SayHi(){
+    console.log("Hey Shubham")
+}
+
+function SayHi2(){
+    console.log("Hey Shivani ")
+}
+
+module.exports = {
+    SayHi, SayHi2
+}
